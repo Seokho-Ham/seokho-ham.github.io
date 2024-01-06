f9fb1a3b53626226dcbf8848d711f4173c2b3c1f
SeokHo-Ham <forkyy.dev@gmail.com>
2024-01-07 01:28:28 +0900

    Updates

--- sw.js
+++ sw.js
@@ -27,20 +27,20 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d3aeb3f07a4870e7d0dd.js"
+    "url": "webpack-runtime-a80213f497f620a3a239.js"
   },
   {
-    "url": "styles.c71dbba85004acb11767.css"
+    "url": "styles.3e211cee019203cf01b5.css"
   },
   {
-    "url": "framework-d0c4743336a0e9c6dd76.js"
+    "url": "framework-47d5efdc17fc7da6c94a.js"
   },
   {
-    "url": "app-61fb624188c497d12423.js"
+    "url": "app-6a4fd399aa345c302a83.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "196d7828abf570da11b9a4e0dc9545d5"
+    "revision": "488d6215273bc2a1baa33811aab7a183"
   },
   {
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmSU5fBBc4.woff2"
@@ -52,11 +52,11 @@ self.__precacheManifest = [
     "url": "static/webfonts/s/roboto/v30/KFOlCnqEu92Fr1MmEU9fBBc4.woff2"
   },
   {
-    "url": "polyfill-cf2ed616d8db78d342fe.js"
+    "url": "polyfill-3aa9aacb6bcdf54ee65a.js"
   },
   {
     "url": "manifest.webmanifest",
-    "revision": "5c567e453235ec9bc544d1e224e8acb3"
+    "revision": "d60efbd0c41e0ea9467e72238dc5586c"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
@@ -166,7 +166,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-61fb624188c497d12423.js`))) {
+  if (!resources || !(await caches.match(`/app-6a4fd399aa345c302a83.js`))) {
     return await fetch(event.request)
   }
 
