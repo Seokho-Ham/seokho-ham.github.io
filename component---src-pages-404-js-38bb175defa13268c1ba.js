5016df16c21c333a9e7b915a5362daa797248002
SeokHo-Ham <forkyy.dev@gmail.com>
2024-09-04 18:37:18 +0900

    Updates

--- component---src-pages-404-js-38bb175defa13268c1ba.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkseokho_ham=self.webpackChunkseokho_ham||[]).push([[883],{4393:function(e,t,n){n.r(t);var u=n(7294),l=n(8093),a=n(579);t.default=function(){return u.createElement(l.Z,null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-38bb175defa13268c1ba.js.map
